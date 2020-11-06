dd9974acd9bf29b8a40b894d8d37f12d34967c5a
郝海亮 <haohailiang@mininglamp.com>
2020-11-06 17:06:55 +0800

    树图

---
+++ practice/13-combo-实战-online/data.js
@@ -0,0 +1,384 @@
+const data = {
+    nodes: [
+        {
+            id: '26',
+            size: 30,
+            label: 'Rex'
+        },
+        {
+            id: '0',
+            label: '10+',
+            comboId: 'a',
+            size: 32,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                style: {
+                    fontSize: 12,
+                    fill: '#ffffff',
+                },
+            },
+        },
+        {
+            id: '1',
+            label: '1',
+            comboId: 'a',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '2',
+            label: '2',
+            comboId: 'a',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '3',
+            label: '3',
+            comboId: 'a',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '4',
+            label: '4',
+            comboId: 'a',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '5',
+            label: '5',
+            comboId: 'a',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '6',
+            label: '6',
+            comboId: 'a',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '27',
+            label: '10+',
+            comboId: 'b',
+            size: 32,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                style: {
+                    fontSize: 12,
+                    fill: '#ffffff',
+                },
+            },
+        },
+        {
+            id: '16',
+            comboId: 'b',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '17',
+            comboId: 'b',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '18',
+            comboId: 'b',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '19',
+            comboId: 'b',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '33',
+            label: '10+',
+            comboId: 'c',
+            size: 32,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                style: {
+                    fontSize: 12,
+                    fill: '#ffffff',
+                },
+            },
+        },
+        {
+            id: '28',
+            comboId: 'c',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '29',
+            comboId: 'c',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '30',
+            comboId: 'c',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '31',
+            comboId: 'c',
+            size: 8,
+            style: {
+                fill: '#86889D',
+                lineWidth: 0,
+            },
+            labelCfg: {
+                refY: 10,
+                position: 'bottom',
+                style: {
+                    fontSize: 12,
+                    fill: '#111111',
+                    opacity: 0.65,
+                },
+            },
+        },
+        {
+            id: '32',
+            label: 'haha',
+            // size: 30,
+            type: 'circle',
+            size: [160],
+            color: '#5B8FF9',
+            style: {
+                fill: '#9EC9FF',
+                lineWidth: 3,
+            },
+            labelCfg: {
+                style: {
+                    fill: '#1890ff',
+                    fontSize: 24,
+                },
+                position: 'bottom',
+            },
+            icon: {
+                // whether show the icon
+                show: true,
+                // icon's img address, string type
+                img:
+                    'https://gw.alipayobjects.com/zos/basement_prod/012bcf4f-423b-4922-8c24-32a89f8c41ce.svg',
+                width: 60,
+                height: 60,
+            },
+        },
+    ],
+    edges: [
+        {
+            source: 'a',
+            target: '26',
+            label: 'a-26',
+        },
+        {
+            source: 'b',
+            target: '26',
+            label: 'b-26',
+        },
+        {
+            source: 'c',
+            target: '26',
+            label: 'c-26',
+        },
+        {
+            source: '32',
+            target: '26',
+            label: '32-26',
+        },
+        {
+            source: '22',
+            target: '23',
+            label: '22-23',
+        },
+    ],
+    combos: [
+        {
+            id: 'a',
+            label: 'Combo A',
+        },
+        {
+            id: 'b',
+            label: 'Combo B',
+        },
+        {
+            id: 'c',
+            label: 'Combo C',
+        },
+    ],
+};
---
+++ practice/13-combo-实战-online/rawData.js
@@ -0,0 +1,494 @@
+const contactsGraphData = {
+    "entities": [
+        {
+            "backColor": "#3A9CAA",
+            "icon": -1,
+            "typeName": "员工",
+            "titleProp": [
+                {
+                    "dataType": "string",
+                    "en": "StaffIDGONGNumHUOMailbox",
+                    "cn": "员工ID-工号或邮箱",
+                    "value": "ID5314624",
+                    "multi": false
+                }
+            ],
+            "label": "Staff",
+            "groupProp": [
+                {
+                    "dataType": "string",
+                    "en": "StaffIDGONGNumHUOMailbox",
+                    "cn": "员工ID-工号或邮箱",
+                    "value": "ID5314624",
+                    "multi": false
+                }
+            ],
+            "id": "4096168008",
+            "labelName": "员工",
+            "type": "Staff",
+            "key": "ID5314624_Staff"
+        },
+        {
+            "backColor": "#3A9CAA",
+            "icon": -1,
+            "typeName": "客户个人",
+            "titleProp": [
+                {
+                    "dataType": "string",
+                    "en": "wxid",
+                    "cn": "微信id",
+                    "value": "13811154957",
+                    "multi": false
+                }
+            ],
+            "label": "ClientPersonal",
+            "groupProp": [
+                {
+                    "dataType": "string",
+                    "en": "wxid",
+                    "cn": "微信id",
+                    "value": "13811154957",
+                    "multi": false
+                }
+            ],
+            "id": "4096155688",
+            "labelName": "客户个人",
+            "type": "ClientPersonal",
+            "key": "13811154957_ClientPersonal"
+        }
+    ],
+    "relations": [
+        {
+            // "isVirtualRelation": true,
+            "isVirtualRelation": false,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "标签平台数据分析",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "易动力",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "时空盒数字",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "盟新",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "富罳",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "黄石金承",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "华成育卓",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "华泰通安",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "惠派国际公司",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "济南亿次元",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "快讯",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "网新恒天",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "图龙信息",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "和泰",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "联软",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "商软冠联",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        },
+        {
+            "isVirtualRelation": true,
+            "titleProp": [],
+            "label": "Project",
+            "subjectId": "4096155688",
+            "graphName": "scopa",
+            "objectKey": "ID5314624_Staff",
+            "id": "3rhjuv4-cn4-9frp-9ns",
+            "groupProp": [],
+            "projectName": "scopa",
+            "labelName": "趋势",
+            "subjectKey": "13811154957_ClientPersonal",
+            "key": "Project::13811154957_ClientPersonal::ID5314624_Staff",
+            "objectId": "4096168008"
+        }
+    ]
+}
+
+const subjectBgColor = '#d8d8d8'
+const subjectBorderColor = '#fd960c'
+const subjectLabelColor = '#000000'
+const objectColor = '#ffffff'
+const objectBgColor = '#86889d'
+
+/**
+ * 格式化内部联系人图谱信息
+ * @param contactsGraphData 原始的图谱信息
+ */
+function getFormatContactsGraphData(contactsGraphData) {
+    const { entities = [], relations = [] } = contactsGraphData
+
+    if (relations.length === 0) {
+        return { nodes: [], edges: [] }
+    }
+
+    // combos
+    const combos = Array.from(
+        new Set(relations.filter(item => item.isVirtualRelation).map(item => item.labelName))
+    ).map(item => ({ id: item, label: item }))
+
+    console.log('%c combos===', 'color:#fff;background: red;', combos)
+
+    // 主体
+    const { subjectId } = relations[0]
+    const subjectEntity = entities.find(item => item.id === subjectId)
+    const subjectNode = {
+        id: subjectEntity.id,
+        nodeType: 'subject',
+        label: subjectEntity.titleProp[0].value,
+        type: 'circle',
+        size: [60],
+        style: {
+            fill: subjectBgColor,
+            stroke: subjectBorderColor,
+            lineWidth: 3,
+        },
+        labelCfg: {
+            style: {
+                fill: subjectLabelColor,
+                fontSize: 24,
+            },
+            position: 'bottom',
+        },
+        icon: {
+            show: subjectEntity.icon !== -1,
+            img: subjectEntity.icon,
+            width: 56,
+            height: 56
+        }
+    }
+
+    // edges
+    const comboEdges = combos.map(comboItem => ({
+        id: `${subjectEntity.id}-${comboItem.id}`,
+        source: subjectEntity.id,
+        target: comboItem.id,
+    }))
+
+    console.log('%c comboEdges===', 'color:#fff;background: red;', comboEdges)
+
+    const realEdges = relations.filter(item => !item.isVirtualRelation)
+        .map(readEdge => ({
+            id: `${subjectEntity.id}-${readEdge.objectId}`,
+            source: subjectEntity.id,
+            target: readEdge.objectId,
+            label: readEdge.labelName,
+            fontSize: 14,
+        }))
+
+    console.log('%c realEdges===', 'color:#fff;background: red;', realEdges)
+
+    // 客体
+    const objectNodes = []
+    let soleNodes = []
+
+    entities
+        .filter(item => item.id !== subjectId)
+        .forEach(objectItem => {
+            const joinCollection = relations
+                .filter(item => item.isVirtualRelation)
+                .filter(item => item.subjectId === subjectEntity.id && item.objectId === objectItem.id) ?? []
+            const soleCollection = relations
+                .filter(item => !item.isVirtualRelation)
+                .filter(item => item.subjectId === subjectEntity.id && item.objectId === objectItem.id) ?? []
+
+            const baseObjectNode = {
+                // id: objectItem.id,
+                nodeType: 'object',
+                label: objectItem.labelName,
+                type: 'circle',
+                // comboId: '关注目标',
+                size: 8,
+                style: {
+                    fill: objectBgColor,
+                    lineWidth: 0
+                },
+                labelCfg: {
+                    refY: 10,
+                    position: 'bottom',
+                    style: {
+                        fontSize: 12,
+                        fill: '#111111',
+                        opacity: 0.65,
+                    },
+                },
+                icon: {
+                    show: false,
+                    img: subjectEntity.icon,
+                }
+            }
+
+            joinCollection.forEach(joinItem => {
+                const { labelName: groupName } = joinItem
+                const tempId = `${groupName}-${joinItem.objectId}`
+                const objectName = entities.find(item => item.id === joinItem.objectId).titleProp[0].value
+
+                objectNodes.push({
+                    ...baseObjectNode, id: tempId, comboId: groupName, label: objectName
+                })
+            })
+
+            soleCollection.forEach(soleItem => {
+                const tempId = `${soleItem.objectId}`
+                objectNodes.push({
+                    ...baseObjectNode, id: tempId, label: soleItem.label, size: 30,
+                })
+            })
+
+            soleNodes = soleCollection.map(item => item.objectId)
+        })
+
+    // 同类客体节点数统计
+    const objectNodesStat = objectNodes.reduce((total, curNode) => {
+        const { comboId } = curNode
+
+        if (comboId) {
+            const prevNum = total[comboId] || 0
+            return { ...total, [comboId]: prevNum + 1 }
+        }
+
+        return total
+    }, {})
+    const statNodes = Object.keys(objectNodesStat).map((curComboId, curComboIndex) => {
+        const label = objectNodesStat[curComboId] > 99 ? '99+' : String(objectNodesStat[curComboId])
+        return {
+            id: `${curComboId}-stat-${curComboIndex}`,
+            nodeType: 'stat',
+            label,
+            type: 'circle',
+            size: 32,
+            style: {
+                fill: objectBgColor,
+                lineWidth: 0
+            },
+            labelCfg: {
+                style: {
+                    fontSize: 12,
+                    fill: objectColor,
+                }
+            },
+            icon: {
+                show: false,
+            },
+            comboId: curComboId,
+        }
+    })
+
+    const nodes = [subjectNode, ...objectNodes, ...statNodes]
+
+    return { nodes, combos, edges: [...comboEdges, ...realEdges], soleNodes, }
+}
+
+const data = getFormatContactsGraphData(contactsGraphData)
