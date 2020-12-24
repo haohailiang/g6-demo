9c83e8260bc2065b496096f83a5c9afb09e280c6
郝海亮 <haohailiang@mininglamp.com>
2020-12-24 14:22:11 +0800

    update

---
+++ examples/02-一般图/13-Concentric同心圆布局/mockData-1.js
@@ -0,0 +1,176 @@
+const data = {
+	'nodes': [
+		{
+			'id': 'node-0',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-1',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-2',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-3',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-4',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-5',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-6',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-7',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-8',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		},
+		{
+			'id': 'node-9',
+			'label': '',
+			// 'type': 'CircleNode',
+			'style': {
+				// 'nodeSize': 24
+			}
+		}
+	],
+	'edges': [
+		{
+			'source': 'node-0',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-1',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-2',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-3',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-4',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-5',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-6',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-7',
+			'label': '',
+		},
+		{
+			'source': 'node-0',
+			'target': 'node-8',
+			'label': '',
+		},
+		{
+			'source': 'node-1',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-2',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-3',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-4',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-5',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-6',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-7',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-8',
+			'target': 'node-0',
+			'label': '',
+		},
+		{
+			'source': 'node-9',
+			'target': 'node-0',
+			'label': '',
+		}
+	]
+}
